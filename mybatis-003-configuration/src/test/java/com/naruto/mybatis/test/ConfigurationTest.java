c2077f9ca72034e681938f3bc370154603d65e2e
naruto055 <1935994680@qq.com>
2024-11-04 23:29:04 +0800

    测试mybatis中<environment>标签和连接池的使用

---
+++ mybatis-003-configuration/src/test/java/com/naruto/mybatis/test/ConfigurationTest.java
@@ -0,0 +1,50 @@
+package com.naruto.mybatis.test;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @Author: naruto
+ * @CreateTime: 2024-11-03-23:52
+ */
+public class ConfigurationTest {
+
+    @Test
+    public void testEnvironment() throws IOException {
+        // 获取SqlSessionFactory对象
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("mybatis-config.xml"));
+        SqlSession sqlSession = sessionFactory.openSession();
+
+        sqlSession.insert("car.insertCar");
+
+        sqlSession.commit();
+        sqlSession.close();
+
+        SqlSessionFactory sessionFactory1 = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("mybatis-config.xml"), "mybatisDB");
+        SqlSession sqlSession1 = sessionFactory1.openSession();
+        sqlSession1.insert("car.insertCar");
+        sqlSession1.commit();
+        sqlSession1.close();
+    }
+
+    @Test
+    public void testPooled() throws IOException {
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+        SqlSession sqlSession = sessionFactory.openSession();
+        sqlSession.insert("car.insertCar");
+        sqlSession.commit();
+        sqlSession.close();
+
+        SqlSession sqlSession2 = sessionFactory.openSession();
+        sqlSession2.insert("car.insertCar");
+        sqlSession2.commit();
+        sqlSession2.close();
+    }
+}
